79847a537a8d2a3de1d608e77f89edd2703353b8
VtotheK <ville.keituri1@gmail.com>
2022-01-01 12:54:14 -0800

    screen install

--- installdepts.sh
+++ installdepts.sh
@@ -1,3 +1,3 @@
 echo "Installing dependencies."
 sleep 2
-sudo apt-get -y install git python3 python3-pip gcc g++ tree build-essential nasm gdb
+sudo apt-get -y install git python3 python3-pip gcc g++ tree build-essential nasm gdb screen
